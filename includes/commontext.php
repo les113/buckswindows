f03f5f7278c36da47ef9da9d147c4eff9f6cfdd7
les113 <leslumb@yahoo.co.uk>
2016-08-12 14:29:09 +0100

    deleted unnecessary file

--- includes/commontext.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$telno='01296 326306';
-$email='enquiry@buckswindows.com';
-$contactus='Contact Buckinghamshire Windows for a professional survey to ensure the best possible installation.';
-?>
